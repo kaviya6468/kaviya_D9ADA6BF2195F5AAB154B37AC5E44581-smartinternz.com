dd5347b986a7cd7245bbfb9744a185fa401bc885
kaviya6468 <144542525+kaviya6468@users.noreply.github.com>
2023-10-08 12:12:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,79 @@
-# Leap Year 
+class BankAccount:
 
-"""
-Year % 4 == 0&
-Year % 100! =0
-Year % 400 == 0
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
 
-"""
-def isLeapYear(Year):
-  if (Year % 4 == 0 and Year % 100
-      != 0) or Year %  400 == 0:
-    return True
-  else:
-    return False
+        self.__account_number = account_number
+
+        self.__account_holder_name = account_holder_name
+
+        self.__account_balance = initial_balance
 
 
-Year = int(input("Enter a Year:"))
 
-if isLeapYear(Year):
-  print('{} is Leap Year.'.format(Year))
-else:
-  print('{} is not a Leap Year.'.format(Year))
-  
+    def deposit(self, amount):
+
+        if amount > 0:
+
+            self.__account_balance += amount
+
+            print(f"Deposited ${amount:.2f} into account {self.__account_number}")
+
+        else:
+
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+
+
+    def withdraw(self, amount):
+
+        if amount > 0:
+
+            if self.__account_balance >= amount:
+
+                self.__account_balance -= amount
+
+                print(f"Withdrew ${amount:.2f} from account {self.__account_number}")
+
+            else:
+
+                print("Insufficient balance. Cannot withdraw.")
+
+        else:
+
+            print("Invalid withdrawal amount. Please withdraw a positive amount.")
+
+
+
+    def display_balance(self):
+
+        print(f"Account {self.__account_number} balance: ${self.__account_balance:.2f}")
+
+
+
+
+
+# Testing the BankAccount class
+
+if __name__== "__main__":
+
+    # Create a BankAccount instance
+
+    account1 = BankAccount("123456", "John Doe", 1000.0)
+
+
+
+    # Deposit money
+
+    account1.deposit(500.0)
+
+
+
+    # Withdraw money
+
+    account1.withdraw(200.0)
+
+
+
+    # Display balance
+
+    account1.display_balance()
